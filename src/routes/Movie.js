37e753c24c97ae8a5232ae6edc43d975f40d5e3c
Sang Hoon Kim <KSHoon522@gamil.com>
2023-12-12 11:28:23 +0900

    useQuery Variables / Apollo dev tools

--- src/routes/Movie.js
+++ src/routes/Movie.js
@@ -1,3 +1,24 @@
+import { gql, useQuery } from "@apollo/client";
+import { useParams } from "react-router-dom";
+
+const GET_MOVIE = gql`
+  query getMovie($movieID: String!) {
+    movie(id: $movieID) {
+      id
+      title
+      small_cover_image
+    }
+  }
+`;
+
 export default function Movie() {
-  return <div>This is movie detail</div>;
+  const { id } = useParams();
+  const { data, loading } = useQuery(GET_MOVIE, {
+    variables: { movieID: id },
+  });
+  console.log(data, loading);
+  if (loading) {
+    return <h1>Fetching moive...</h1>;
+  }
+  return <h1>{data.movie.title}</h1>;
 }
--- src/routes/Movies.js
+++ src/routes/Movies.js
@@ -1,26 +1,37 @@
-import { gql, useApolloClient } from "@apollo/client";
-import { useEffect, useState } from "react";
+import { gql, useQuery } from "@apollo/client";
+import { Link } from "react-router-dom";
+
+const ALL_MOVIES = gql`
+  query getMovies {
+    allMovies {
+      title
+      id
+    }
+    allTweets {
+      id
+      text
+      author {
+        fullName
+      }
+    }
+  }
+`;
 
 export default function Movies() {
-  const [movies, setMovies] = useState([]);
-  const client = useApolloClient();
-  useEffect(() => {
-    client
-      .query({
-        query: gql`
-          {
-            allMovies {
-              title
-            }
-          }
-        `,
-      })
-      .then((result) => setMovies(result.data.allMovies));
-  }, [client]);
+  const { data, loading, error } = useQuery(ALL_MOVIES);
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
+  if (error) {
+    return <h1>Could not fetch</h1>;
+  }
   return (
     <ul>
-      {movies.map((movie) => (
-        <li key={movie.id}>{movie.title}</li>
+      <h1>Movies</h1>
+      {data.allMovies.map((movie) => (
+        <li key={movie.id}>
+          <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
+        </li>
       ))}
     </ul>
   );
